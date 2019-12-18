c2627591ce52f210c945c21637bb8512a9c8285d
fawaz70 <fawaz_pirzada@hotmail.com>
2019-12-18 16:48:48 -0500

    made a few changes, can't figure out how to fill page 2

--- app/src/main/java/com/example/mobilecalculator/GradeCalculator.java
+++ app/src/main/java/com/example/mobilecalculator/GradeCalculator.java
@@ -1,7 +1,9 @@
 package com.example.mobilecalculator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +13,8 @@ public class GradeCalculator extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_grade_calculator);
+    }
+    public void BackButton1(View back1){
+        startActivity(new Intent(GradeCalculator.this, MainActivity.class));
     }
 }
