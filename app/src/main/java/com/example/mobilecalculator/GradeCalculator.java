73ea03dd8d4f0ca6bfd6b6c6cd69ecd389e6b4e8
fawaz70 <fawaz_pirzada@hotmail.com>
2019-12-27 04:17:36 -0500

    fixed page and made the view, just need to do sum

--- app/src/main/java/com/example/mobilecalculator/GradeCalculator.java
+++ app/src/main/java/com/example/mobilecalculator/GradeCalculator.java
@@ -1,20 +1,14 @@
 package com.example.mobilecalculator;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.view.View;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
 public class GradeCalculator extends AppCompatActivity {
+
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grade_calculator);
-    }
-    public void BackButton1(View back1){
-        startActivity(new Intent(GradeCalculator.this, MainActivity.class));
     }
 }
